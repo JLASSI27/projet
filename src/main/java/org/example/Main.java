76cd88f98678b63384513efdf84837f1881da4d4
MOUMNIMEHER <156126895+moumnimeher@users.noreply.github.com>
2024-06-04 21:41:03 +0100

    Merge pull request #2 from JLASSI27/mahermoumni
    mahermoumni

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
 
+import entities.Seance;
 import util.Datasource;
-
+import java.util.Date;
 import java.sql.Connection;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Datasource ds = new Datasource();
-        Connection cnx=ds.getCon();
 
 
     }
