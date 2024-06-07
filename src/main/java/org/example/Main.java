8709240904e415aa6eefa635d6820125e918d928
MalekDev420 <malek.benkacem@esprit.tn>
2024-06-07 09:42:00 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 
+import Services.AbonnementService;
 import Services.UtilisateurService;
 import util.DataSource;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UtilisateurService utilisateurService = new UtilisateurService();
+        AbonnementService abonnementService = new AbonnementService();
 
         try (Connection connection = DataSource.getInstance().getConnexion()) {
             // Création d'un nouvel utilisateur
@@ -30,10 +29,10 @@ public class Main {
             utilisateurService.ajouter(connection, nouvelUtilisateur);
 
             // Suppression de l'utilisateur
-            utilisateurService.supprimer(41);
+            utilisateurService.supprimer(1);
 
             // Consultation de l'utilisateur
-            org.example.Utilisateur utilisateur = utilisateurService.consulter(36);
+            org.example.Utilisateur utilisateur = utilisateurService.consulter(2);
             if (utilisateur != null) {
                 System.out.println("Utilisateur consulté : " + utilisateur.getNomU());
             }
@@ -47,14 +46,14 @@ public class Main {
             System.out.println("Utilisateurs triés : " + utilisateursTries.size());
 
             // Vérification de l'ID
-            boolean idExiste = utilisateurService.verifId(5);
+            boolean idExiste = utilisateurService.verifId(2);
             System.out.println("ID existe : " + idExiste);
 
             // Mettre à jour le mot de passe
-            utilisateurService.oublierMdp(15, "7856697");
+            utilisateurService.oublierMdp(3, "7856697");
 
             // Modification de l'utilisateur
-            int idUtilisateurAModifier = 10; // Remplacez 10 par l'ID de l'utilisateur que vous souhaitez modifier
+            int idUtilisateurAModifier = 4; // Remplacez 4 par l'ID de l'utilisateur que vous souhaitez modifier
             org.example.Utilisateur utilisateurModifie = new org.example.Utilisateur();
             utilisateurModifie.setNomU("Malek");
             utilisateurModifie.setPrenomU("Benkacem");
@@ -65,11 +64,50 @@ public class Main {
             utilisateurModifie.setDateNaissance(new Date());
 
             utilisateurService.modifier(connection, idUtilisateurAModifier, utilisateurModifie);
+
+            // Ajout d'un nouvel abonnement
+            Abonnement newAbonnement = new Abonnement();
+            newAbonnement.setMontant(100.0f);
+            newAbonnement.setDateExpiration(new Date()); // Utilisez dateExpiration au lieu de dateExpedition
+            newAbonnement.setCodePromo("PROMO2023");
+            newAbonnement.setTypeAbonnement("Premium");
+            newAbonnement.setIdU(nouvelUtilisateur.getIdU());
+
+            abonnementService.ajouterS(newAbonnement);
+
+            // Modifier un abonnement
+            Abonnement modifiedAbonnement = new Abonnement();
+            modifiedAbonnement.setMontant(200.0f);
+            modifiedAbonnement.setDateExpiration(new Date()); // Utilisez dateExpiration au lieu de dateExpedition
+            modifiedAbonnement.setCodePromo("PROMO2024");
+            modifiedAbonnement.setTypeAbonnement("Gold");
+            modifiedAbonnement.setIdU(nouvelUtilisateur.getIdU());
+
+            abonnementService.modifierS(newAbonnement.getIdA(), modifiedAbonnement);
+
+            // Consulter un abonnement
+            Abonnement abonnement = abonnementService.consulter(newAbonnement.getIdA());
+            if (abonnement != null) {
+                System.out.println("Abonnement consulté : " + abonnement.getTypeAbonnement());
+            }
+
+            // Supprimer un abonnement
+            abonnementService.supprimerS(newAbonnement.getIdA());
+
+            // Abonnement personnalisé
+            abonnementService.abonnementPersonnalise(newAbonnement.getIdA(), "NEWPROMO2024");
+
+            // Rappel automatique
+            abonnementService.rappelAutomatique();
+
+            // Paiement abonnement
+            abonnementService.paiementAbonnement(newAbonnement.getIdA());
+
+            // Notification abonnement
+            abonnementService.notificationAbonnement(newAbonnement.getIdA());
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
     }
-
 }
