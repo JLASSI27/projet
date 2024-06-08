3dadae417dba8e3ed1e0ab58210c52c61c6cdcb8
hamrounimouna <mouna.hamrouni@esprit.tn>
2024-06-08 14:01:42 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-
+        System.out.println("4. Afficher reclamation by id");
     }
 }
