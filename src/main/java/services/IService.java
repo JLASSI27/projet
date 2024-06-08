33bccaea6b4207be82497bc3f576a1138fdb30ae
MOUMNIMEHER <156126895+MOUMNIMEHER@users.noreply.github.com>
2024-06-08 15:25:22 +0100

    Update IService.java

--- src/main/java/services/IService.java
+++ src/main/java/services/IService.java
@@ -11,6 +11,7 @@ public interface IService <T>{
 
     public void ajouter (T x) throws SQLException;
     public void modifier(T x) throws SQLException;
+
     public void supprimer(int id) throws SQLException;
     public T getOneById(int id)throws SQLException;
     public List<T> getAll() throws SQLException;
