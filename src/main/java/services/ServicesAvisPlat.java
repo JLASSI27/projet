8e920704c1ce7436aaf49cea48e90a99e021ae3c
MOUMNIMEHER <156126895+MOUMNIMEHER@users.noreply.github.com>
2024-06-08 15:25:43 +0100

    Update ServicesAvisPlat.java

--- src/main/java/services/ServicesAvisPlat.java
+++ src/main/java/services/ServicesAvisPlat.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.scanner
 public class ServicesAvisPlat implements IService<AvisPlat> {
   private Connection con = Datasource.getInstance().getCon();
 
